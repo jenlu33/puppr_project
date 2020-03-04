ec8ad8c8fa6e761475c6aaed828a1aa5877c299f
jenlu33 <nejul33@gmail.com>
2020-03-03 21:59:49 -0800

    work on user auth

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_225447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "photos", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "album_id", null: false
-    t.string "title", null: false
-    t.text "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
