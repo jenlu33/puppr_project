f0801c8024851243f604c6f65aeff94fbc16aeef
jenlu33 <nejul33@gmail.com>
2020-04-07 10:44:22 -0700

    Write functions for comment create and destroy

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,11 +1,24 @@
 class Api::CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-
+    @comment.user_id = current_user.id
+    @comment.photo_id = params[:photo_id]
+    
     if @comment.save
       render @comment
     else
       render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+    else
+      render json @comment.errors.full_messages, status: 422
     end
   end
 
