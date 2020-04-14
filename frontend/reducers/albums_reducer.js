d25a9b7be355911b436eb3fe4a3db0a8c86e1a65
jenlu33 <nejul33@gmail.com>
2020-04-13 20:15:50 -0700

    Create albums reducer

---
+++ frontend/reducers/albums_reducer.js
@@ -0,0 +1,24 @@
+import { 
+  RECEIVE_ALL_ALBUMS, 
+  RECEIVE_ALBUM, 
+  REMOVE_ALBUM 
+} from '../actions/album_actions';
+
+const albumsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ALL_ALBUMS:
+      return Object.assign({}, state, action.state);
+    case RECEIVE_ALBUM:
+      return Object.assign({}, state, {[action.album.id]: action.album});
+    case REMOVE_ALBUM:
+      let newState = Object.assign({}, state);
+      delete newState[action.albumId];
+      return newState;
+    default:
+      return state;
+  };
+};
+
+export default albumsReducer;
